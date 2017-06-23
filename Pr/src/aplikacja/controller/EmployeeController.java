94dda650760b6769b3c6bcf7fa81d6c499c8890c
Bartosz <bartosz.logwiniuk@gmail.com>
2017-06-23 12:38:31 +0200

    Update EmployeeController.java

--- Pr/src/aplikacja/controller/EmployeeController.java
+++ Pr/src/aplikacja/controller/EmployeeController.java
@@ -62,24 +62,22 @@ public class EmployeeController implements Initializable{
     private TextField textNetSalary;
     
     public DbController db;
- 
     public ObservableList<Employee>data=FXCollections.observableArrayList();
    
    
 
     public void btnRefreshAction(ActionEvent event) throws SQLException, ClassNotFoundException{
     	try{
-    		Connection conn = db.Connection();
+    	    Connection conn = db.Connection();
             data = FXCollections.observableArrayList();       
             ResultSet rs = conn.createStatement().executeQuery("SELECT * from employee");
             while (rs.next()){
-                data.add(new Employee(rs.getString(2), rs.getString(3), rs.getString(4),rs.getString(5)));
+            	data.add(new Employee(rs.getString(2), rs.getString(3), rs.getString(4),rs.getString(5)));
             }
             
         }catch (SQLException ex){
             System.err.print("Error" + ex);
-        }
-        
+        }      
         FirstNameTwo.setCellValueFactory(new PropertyValueFactory<Employee,String>("firstName"));
         LastNameTwo.setCellValueFactory(new PropertyValueFactory<Employee,String>("lastName"));
         GrossSalaryTwo.setCellValueFactory(new PropertyValueFactory<Employee,String>("salary1"));
@@ -90,34 +88,26 @@ public class EmployeeController implements Initializable{
     }
     
     @FXML
-    private void btnSaveChangesAction(ActionEvent event) throws ClassNotFoundException, SQLException {
-        
-        java.sql.PreparedStatement preparedStatement = null;
-        
+    private void btnSaveChangesAction(ActionEvent event) throws ClassNotFoundException, SQLException {        
+        java.sql.PreparedStatement preparedStatement = null;       
         String surname = TableTwo.getSelectionModel().getSelectedItem().getLastName();
         Connection conn = db.Connection();
-            try {
+        try {
 		String sql = "UPDATE employee SET firstName_employee = ?, lastName_employee = ?, gross_earnings = ?, net_eartnings = ?" 
-                              + " WHERE lastName_employee = ?";
-		
+                + " WHERE lastName_employee = ?";	
 		preparedStatement = conn.prepareStatement(sql);	    		        
 		preparedStatement.setString(1, textFirstName.getText());
 		preparedStatement.setString(2, textLastName.getText());
 		preparedStatement.setString(3, textGrossSalary.getText());
 		preparedStatement.setString(4, textNetSalary.getText());
-                preparedStatement.setString(5, surname);
-		    		        		
+                preparedStatement.setString(5, surname);		    		        		
 		preparedStatement.executeUpdate();
-		
-		
-                
+		    
             } catch (SQLException ex) {
 		System.err.print("Error " + ex);
-            }
-        
+            }       
         try{
-            data = FXCollections.observableArrayList();
-         
+            data = FXCollections.observableArrayList();         
             ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM employee ORDER BY lastName_employee");
             while (rs.next()){
                 data.add(new Employee(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
@@ -135,41 +125,36 @@ public class EmployeeController implements Initializable{
     }   
     
     @FXML
-    private void btnSaveEmployeeAction(ActionEvent event) throws ClassNotFoundException {
-        
+    private void btnSaveEmployeeAction(ActionEvent event) throws ClassNotFoundException {      
         java.sql.PreparedStatement preparedStatement = null;
-
         try {
             Connection conn = db.Connection();
             String sql = "INSERT INTO employee (firstName_employee, lastName_employee, gross_earnings, net_eartnings)"
                         + " values (?, ?, ?, ?)";
-            preparedStatement = conn.prepareStatement(sql);
-            
-    		preparedStatement.setString(1, textFirstName.getText());
-    		preparedStatement.setString(2, textLastName.getText());
-    		preparedStatement.setString(3, textGrossSalary.getText());
-    		preparedStatement.setString(4, textNetSalary.getText());
-		    		        		
+            preparedStatement = conn.prepareStatement(sql);         
+    	    preparedStatement.setString(1, textFirstName.getText());
+    	    preparedStatement.setString(2, textLastName.getText());
+    	    preparedStatement.setString(3, textGrossSalary.getText());
+    	    preparedStatement.setString(4, textNetSalary.getText());	    		        		
             preparedStatement.executeUpdate();
             
             textFirstName.setText("");
             textLastName.setText("");
             textGrossSalary.setText("");
             textNetSalary.setText("");
-
-                    
+		
             } catch (SQLException ex) {
                 System.err.print("Error " + ex);
             }
             
         try{
             Connection conn = db.Connection();
-            data = FXCollections.observableArrayList();
-         
+            data = FXCollections.observableArrayList();         
             ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM employee");
             while (rs.next()){
                 data.add(new Employee(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
             }
+		
         }catch (SQLException ex){
             System.err.print("Error " + ex);
         }
@@ -182,33 +167,30 @@ public class EmployeeController implements Initializable{
         TableTwo.setItems(data);
     }  
     @FXML
-    private void btnDeleteAction(ActionEvent event) throws ClassNotFoundException, SQLException {
-        
+    private void btnDeleteAction(ActionEvent event) throws ClassNotFoundException, SQLException {        
         java.sql.PreparedStatement preparedStatement = null;
         String surname = TableTwo.getSelectionModel().getSelectedItem().getlastName();
         Connection conn = db.Connection();
         try {
-		String sql = "DELETE FROM  employee WHERE lastName_employee = '" + surname + "'";
-                preparedStatement = conn.prepareStatement(sql);
-		preparedStatement.executeUpdate();
-		
-		textFirstName.setText("");
-        textLastName.setText("");
-        textGrossSalary.setText("");
-        textNetSalary.setText("");
-            
-                
+	    String sql = "DELETE FROM  employee WHERE lastName_employee = '" + surname + "'";
+            preparedStatement = conn.prepareStatement(sql);
+	    preparedStatement.executeUpdate();		
+	    textFirstName.setText("");
+  	    textLastName.setText("");
+            textGrossSalary.setText("");
+            textNetSalary.setText("");
+                           
             }catch (SQLException ex) {
 		System.err.print("Error" + ex);
             }
             
         try{
-            data = FXCollections.observableArrayList();
-         
+            data = FXCollections.observableArrayList();         
             ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM employee ORDER BY lastName_employee");
             while (rs.next()){
                 data.add(new Employee(rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
             }
+		
         }catch (SQLException ex){
             System.err.print("Error " + ex);
         }
@@ -220,8 +202,7 @@ public class EmployeeController implements Initializable{
         TableTwo.setItems(null);
         TableTwo.setItems(data);
     }   
-
-          
+         
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		db = new DbController();	
